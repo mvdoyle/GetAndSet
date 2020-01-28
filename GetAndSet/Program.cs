d0138149dcae25ff576ec5a4072c7de5818deae3
mvdoyle <mdoyle@truecoders.io>
2020-01-28 09:34:27 -0600

    created

---
+++ GetAndSet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GetAndSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+
+            user.Name = "Michael";
+
+            Console.WriteLine(user.Name);
+        }
+    }
+}
---
+++ GetAndSet/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace GetAndSet
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        private string name = "Suresh Dasari";
+
+        public string Name
+        {
+            get
+            {
+                return name.ToUpper();
+            }
+
+            set
+            {
+                if (value == "Michael")
+                    name = value;
+            }
+        }
+
+    }
+}
